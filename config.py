11ca97e1f15c1fda8a2f5cb3d2c6c11331b3462c
tryToCode <dong.yang@campus.tu-berlin.de>
2019-01-03 15:31:14 +0100

    change database uri

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') \
     or 'k\x80\xec\xb5\xb6\x87d\x19\x96%\r]\x189\xff\xe5\xd36\xa8\x15Gk\x947'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="fabian082",
+    password="a15803449527",
+    hostname="fabian082.mysql.pythonanywhere-services.com",
+    databasename="fabian082$dailyGermanDB",
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
