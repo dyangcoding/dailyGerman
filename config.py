796b0d7a702d936eb40fca011b426d6509951a06
tryToCode <dong.yang@campus.tu-berlin.de>
2019-01-05 20:11:19 +0100

    change mail server port and use ssl

--- config.py
+++ config.py
@@ -16,8 +16,7 @@ class Config(object):
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
