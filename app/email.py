c6b7b4b5b20c0637eab70df1085076f19c8c2146
tryToCode <dong.yang@campus.tu-berlin.de>
2019-01-12 07:42:40 +0100

    update requirements.txt

--- app/email.py
+++ app/email.py
@@ -2,19 +2,10 @@ from flask_mail import Message
 from app import mail
 from flask import render_template, current_app
 from threading import Thread
-import yagmail
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
-
-def init_yagmail():
-    yag = yagmail.SMTP(current_app.config['MAIL_USERNAME'], current_app.config['MAIL_PASSWORD'])
-    return yag
-
-def send_mail_yag(subject, msg):
-    yag = init_yagmail()
-    yag.send(subject, msg)
     
 def send_email(subject, sender, recipients, text_body, sync=False):
     msg = Message(subject, sender=sender, recipients=recipients)
