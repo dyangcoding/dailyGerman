3014f87638ffb7d9b06137a55c9d81f404d45646
tryToCode <dong.yang@campus.tu-berlin.de>
2019-01-20 09:45:01 +0100

    change post detail page layout

--- app/email.py
+++ app/email.py
@@ -2,10 +2,20 @@ from flask_mail import Message
 from app import mail
 from flask import render_template, current_app
 from threading import Thread
+import smtplib, ssl
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
+
+def smtp_ssl_send():
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:          
+        server.login(sender_email, password)
+        msg = message2Html('test html sending', html)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
 
 def send_email(subject, sender, recipients, text_body, sync=False):
     msg = Message(subject, sender=sender, recipients=recipients)
