d6f15ee1a636097b487a223b3d0d1f4d679ee9a1
yang <tu295.dy@gmail.com>
2019-05-08 12:38:24 +0000

    icon for back to top button

--- app/static/js/back2Top.js
+++ app/static/js/back2Top.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
     document.getElementById("backToTop").style.display = "block";
   } else {
     document.getElementById("backToTop").style.display = "none";
