16199913e3be2eb2d271b9f9ae8f1fba0c4a9275
tryToCode <dong.yang@campus.tu-berlin.de>
2019-05-25 01:13:42 +0200

    new send email config

--- config.py
+++ config.py
@@ -17,9 +17,10 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
     ADMINS = ['dailygerman9527@gmail.com']
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
